6008ecf8e7bceca29bbca1149f8c627dfd743e76
Eduardo Ferreira <eduardomf10@hotmail.com>
2023-03-09 19:43:29 -0800

    Initial Commit

---
+++ ftvPoints Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ftvPoints Watch App
+//
+//  Created by Eduardo Ferreira on 3/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ftvPoints Watch App/ftvPointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ftvPointsApp.swift
+//  ftvPoints Watch App
+//
+//  Created by Eduardo Ferreira on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ftvPoints_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
